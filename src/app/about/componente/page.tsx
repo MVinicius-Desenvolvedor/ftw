0d819dec6cc32366399a8ae0bdc00ebe6233a409
M Vinicius <contatoonline4000@gmail.com>
2023-07-15 10:07:00 -0300

    initial commit

---
+++ src/app/about/componente/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Trips = () => {
+    return (
+        <div>trips </div>
+
+
+    )
+}
+
+export default Trips
---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Trips from "./componente/page";
+
+
+
+const Page = () => {
+    return (
+        <div>
+            <Trips></Trips>
+
+        </div>
+        
+
+
+    )
+}
+
+export default Page
